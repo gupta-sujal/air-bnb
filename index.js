97eb444d30e39bbe9a59feaa0e6b69dc34243aac
gupta-sujal <sujalgupta484@gmail.com>
2023-06-15 00:04:56 +0530

    included the middle part of the page and the photo-mesh

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
-function navbar()
+function Navbar()
 {
 return (
-    <nav>
-        <img src="./download.img"/>
+    <nav className="navimage">
+        <img src="download.png"/>
     </nav>
 )
 }
+function Middle()
+{
+   return( <div>
+        <img className="mesh" src="photo-mesh.jpg"></img>
+        <h1 className="hero--header">Online Experiences</h1>
+            <p className="hero--text">Join unique interactive activities led by 
+            one-of-a-kind hosts—all without leaving home.</p>
+    </div>
+   )
+}
+function Page()
+{
+    return (
+        <div>
+            <Navbar />
+            <Middle />
+        </div>
+    )
+}
+ReactDOM.render(<Page/>, document.getElementById("root"));
